5d7656ee59438cba726c56e9c29b88a8bfb26aba
Jerry Ren <jerryexplodes@gmail.com>
2020-04-27 13:31:48 -0400

    Emptiness, to be overriden

---
+++ MeiTa InterU IcePac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeiTa InterU IcePac
+//
+//  Created by Jerry Ren on 4/27/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
