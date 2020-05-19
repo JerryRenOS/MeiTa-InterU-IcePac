7f6267eda6f23f37a485f43ecf8273cf21fe3615
Jerry Ren <yren@gm.slc.edu>
2020-05-18 23:55:49 -0400

    scnP detected

--- AlphaRadioactive co./AlphaRadioactive/AlphaRadioactiveViewControl.swift
+++ AlphaRadioactive co./AlphaRadioactive/AlphaRadioactiveViewControl.swift
@@ -16,13 +16,34 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         sceneView.delegate = self
-        manifestSpherical()
+        
+        manifestDiciness()
+  
     }
     
+    // MARK: - Diciness
+    
+    func manifestDiciness() {
+        sceneView.autoenablesDefaultLighting = true
+        let diceyScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceyNode = diceyScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceyNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+            sceneView.scene.rootNode.addChildNode(diceyNode)
+            
+            let rotationAction = SCNAction.rotate(by: -360 * CGFloat(Double.pi / 90), around: SCNVector3(0, 0.1, 0), duration: 17)
+            diceyNode.runAction(rotationAction)
+        }
+    }
+    
+    
+    
     // MARK: - Spherical
-
+    
     func manifestSpherical() {
         let spherical = SCNSphere(radius: 0.1)
         let spherMaterial = SCNMaterial()
@@ -32,9 +53,9 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
         
         //try  EarthySpecular2k.tif when have chance
         
-
+        
         // can have one or two different buttons on the bottom for different showcase effects
-// (E.g. both diffuse and specular to earthTexture2K and no other settings would have a cool effect)
+        // (E.g. both diffuse and specular to earthTexture2K and no other settings would have a cool effect)
         
         
         spherical.materials = [spherMaterial]
@@ -48,10 +69,10 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.scene.rootNode.addChildNode(spherNode)
         sceneView.autoenablesDefaultLighting = true
-
+        
     }
     
-
+    
     // MARK: - Cubicle
     
     func manifestCubicle() {
@@ -67,23 +88,52 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
     }
     
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let scenePlane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2 , 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            scenePlane.materials = [gridMaterial]
+            planeNode.geometry = scenePlane
+            
+            node.addChildNode(planeNode)
+             
+        } else {
+            return
+        }
+    }
+    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         let configuration = ARWorldTrackingConfiguration()
         
+        configuration.planeDetection = .horizontal
+        
+        
         print(ARWorldTrackingConfiguration.isSupported)
-
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         sceneView.session.pause()
     }
+    
+
+    
+    
 }
- 
+
 
 
 
@@ -92,7 +142,7 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
 //
 //        // Set the scene to the view
 //        sceneView.scene = scene
- 
+
 
 // MARK: - SCNAction
 
