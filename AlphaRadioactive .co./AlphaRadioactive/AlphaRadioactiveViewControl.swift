545c92eaa3e923ba3f556932c48cfa5161737539
Jerry Ren <yren@gm.slc.edu>
2020-05-21 21:28:22 -0400

    diciness

--- AlphaRadioactive .co./AlphaRadioactive/AlphaRadioactiveViewControl.swift
+++ AlphaRadioactive .co./AlphaRadioactive/AlphaRadioactiveViewControl.swift
@@ -22,72 +22,10 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         manifestDiciness()
-  
-    }
-    
-    // MARK: - Diciness
-    
-    func manifestDiciness() {
-        sceneView.autoenablesDefaultLighting = true
-        let diceyScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        
-        if let diceyNode = diceyScene.rootNode.childNode(withName: "Dice", recursively: true) {
-            diceyNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-            sceneView.scene.rootNode.addChildNode(diceyNode)
-            
-            let rotationAction = SCNAction.rotate(by: -360 * CGFloat(Double.pi / 90), around: SCNVector3(0, 0.1, 0), duration: 17)
-            diceyNode.runAction(rotationAction)
-        }
-    }
-    
-    
-    
-    // MARK: - Spherical
-    
-    func manifestSpherical() {
-        let spherical = SCNSphere(radius: 0.1)
-        let spherMaterial = SCNMaterial()
-        spherMaterial.diffuse.contents = UIImage(named: "art.scnassets/EarthyTexture2K.jpg")
-        spherMaterial.specular.contents = UIImage(named: "art.scnassets/EarthyTexture2K.jpg")
-        spherMaterial.normal.contents = UIImage(named: "Normal")
-        
-        //try  EarthySpecular2k.tif when have chance
-        
-        
-        // can have one or two different buttons on the bottom for different showcase effects
-        // (E.g. both diffuse and specular to earthTexture2K and no other settings would have a cool effect)
-        
-        
-        spherical.materials = [spherMaterial]
-        
-        let spherNode = SCNNode()
-        spherNode.position = SCNVector3(0, -0.1, -0.5)
-        spherNode.geometry = spherical
-        
-        let action = SCNAction.rotate(by: -360 * CGFloat(Double.pi / 30), around: SCNVector3(0 ,0.1, 0), duration: 13)
-        spherNode.runAction(action)
-        
-        sceneView.scene.rootNode.addChildNode(spherNode)
-        sceneView.autoenablesDefaultLighting = true
         
     }
     
-    
-    // MARK: - Cubicle
-    
-    func manifestCubicle() {
-        let cubicle = SCNBox(width: 0.08, height: 0.08, length: 0.08, chamferRadius: 0.008)
-        let cubiMaterial = SCNMaterial()
-        cubiMaterial.diffuse.contents = UIColor.gray
-        cubicle.materials = [cubiMaterial]
-        let cubiNode = SCNNode()
-        cubiNode.position = SCNVector3(0, 0.1, -0.5)
-        cubiNode.geometry = cubicle
-        
-        sceneView.scene.rootNode.addChildNode(cubiNode)
-        sceneView.autoenablesDefaultLighting = true
-    }
-    
+
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
             
@@ -104,26 +42,11 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
             planeNode.geometry = scenePlane
             
             node.addChildNode(planeNode)
-             
+            
         } else {
             return
         }
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let firstTouch = touches.first {
-            let firstTouchLocation = firstTouch.location(in: sceneView)
-            
-            let hitResults = sceneView.hitTest(firstTouchLocation, types: .existingPlaneUsingExtent)
-            
-            if !hitResults.isEmpty {
-                 print("plane' been touched")
-            } else {
-                print("touch was outside of the plane")
-            }
-        }
-    }
-    
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -143,7 +66,7 @@ class AlphaRadioactiveViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-
+    
     
     
 }
