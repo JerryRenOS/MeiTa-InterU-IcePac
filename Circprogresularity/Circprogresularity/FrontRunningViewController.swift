06308a42280e199c6172d1bb8b0fa44070165cc0
Jerry Ren <jerryren.ios@gmail.com>
2021-01-19 14:42:53 -0500

    iroiroRGB generatingColorWithRGB + CATransform3DMakeRotation(-CGFloat.pi/2)

--- Circprogresularity/Circprogresularity/FrontRunningViewController.swift
+++ Circprogresularity/Circprogresularity/FrontRunningViewController.swift
@@ -12,7 +12,8 @@ class FrontRunningViewController: UIViewController, URLSessionDownloadDelegate {
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         let byfinbi  = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
         DispatchQueue.main.async {
-            self.circularLayer.strokeEnd = byfinbi / 4.35   // syncing download progress with the progress track (kinda)
+    //        self.circularLayer.strokeEnd = byfinbi / 4.35
+    // syncing download progress with the progress track (not working properly if the download is too fast)
         }
         print(totalBytesWritten, totalBytesExpectedToWrite)
     }
@@ -20,15 +21,19 @@ class FrontRunningViewController: UIViewController, URLSessionDownloadDelegate {
     }
     
     private let circularLayer = CAShapeLayer()
+    private var filledOrNot = true // perform the ternary operation in  future commit
     
     override func viewDidLoad() {
         super.viewDidLoad()
         circularRendering()
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(piTraversal)))
     }
     
     func circularRendering() {
-        let centerOfView = view.center
-        let circularPath = UIBezierPath(arcCenter: centerOfView, radius: 101, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi * 2, clockwise: true)
+//        let centerOfView = view.center
+//        let circularPath = UIBezierPath(arcCenter: centerOfView, radius: 101, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi * 2, clockwise: true)
+        
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 101, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
         
         trackBuilder(bezierPath: circularPath)
         circulationBuilder(using: circularPath)
@@ -36,12 +41,12 @@ class FrontRunningViewController: UIViewController, URLSessionDownloadDelegate {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) { [self] in
             piTraversal()
             circularLayer.fillColor = UIColor.clear.cgColor
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.1) {
                 circularLayer.fillColor = UIColor.black.cgColor
                 circularLayer.lineWidth = 6.5
             }
         }
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(piTraversal)))
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(piTraversal)))
     }
     
     // MARK: -
@@ -49,9 +54,11 @@ class FrontRunningViewController: UIViewController, URLSessionDownloadDelegate {
     private func trackBuilder(bezierPath: UIBezierPath) {
         let layerOfTrack = CAShapeLayer.init()
         layerOfTrack.path = bezierPath.cgPath
-        layerOfTrack.strokeColor = UIColor.black.cgColor
+        let desiredStrokeColor = generatingColorWithRGB(red: 204, green: 238, blue: 255, alpha: 1)
+        layerOfTrack.strokeColor = desiredStrokeColor.cgColor
         layerOfTrack.lineWidth = 8.6
         layerOfTrack.fillColor = .none
+        layerOfTrack.position = view.center    //important
         view.layer.addSublayer(layerOfTrack)
     }
     
@@ -61,6 +68,8 @@ class FrontRunningViewController: UIViewController, URLSessionDownloadDelegate {
         circularLayer.lineCap = .round
         circularLayer.lineWidth = 8.6
         circularLayer.strokeEnd = 0
+        circularLayer.position = view.center
+        circularLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1) //-CGFloat.pi = 逆时针90度
         view.layer.addSublayer(circularLayer)
     }
     
@@ -89,6 +98,7 @@ class FrontRunningViewController: UIViewController, URLSessionDownloadDelegate {
         fundamentalAnimation.duration = 2
         fundamentalAnimation.fillMode = .forwards
         fundamentalAnimation.isRemovedOnCompletion = false
+        circularLayer.fillColor = UIColor.clear.cgColor // future ternary
         circularLayer.add(fundamentalAnimation, forKey: "fundamentalism")
     }
 }
---
+++ Circprogresularity/Circprogresularity/IroIroRGB.swift
@@ -0,0 +1,25 @@
+//
+//  IroIroRGB.swift
+//  Circprogresularity
+//
+//  Created by Jerry Ren on 1/19/21.
+//
+
+import UIKit
+
+//extension UIColor {
+//    convenience init(hexCodeEightDigits:Int, alpha: CGFloat = 1) {
+//        self.init(
+//            red: CGFloat((hexCodeEightDigits & 0xFF0000) >> 16)/255,
+//            green: CGFloat((hexCodeEightDigits & 0x00FF00) >> 8)/255,
+//            blue:  CGFloat((hexCodeEightDigits & 0x0000FF) >> 0)/255, alpha: alpha
+//        ) // for instance: 0xfe5960
+//    }
+//}
+
+extension UIViewController {
+    func generatingColorWithRGB(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: Float) -> UIColor {
+        let colorByRGB = UIColor.init(red: red/255, green: CGFloat(green/255), blue: (blue/255), alpha: CGFloat(alpha))
+        return colorByRGB
+    }
+}
