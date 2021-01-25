74bf0f953c648eeb1db6189baf40849079a09986
Jerry Ren <jerryren.ios@gmail.com>
2021-01-24 23:47:39 -0500

    convenience init hexcode extenze

--- Circprogresularity/Circprogresularity/IroIroRGB.swift
+++ Circprogresularity/Circprogresularity/IroIroRGB.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-//extension UIColor {
-//    convenience init(hexCodeEightDigits:Int, alpha: CGFloat = 1) {
-//        self.init(
-//            red: CGFloat((hexCodeEightDigits & 0xFF0000) >> 16)/255,
-//            green: CGFloat((hexCodeEightDigits & 0x00FF00) >> 8)/255,
-//            blue:  CGFloat((hexCodeEightDigits & 0x0000FF) >> 0)/255, alpha: alpha
-//        ) // for instance: 0xfe5960
-//    }
-//}
+extension UIColor {
+    convenience init(hexCodeEightDigits:Int, alpha: CGFloat = 1) {
+        self.init(
+            red: CGFloat((hexCodeEightDigits & 0xFF0000) >> 16)/255,
+            green: CGFloat((hexCodeEightDigits & 0x00FF00) >> 8)/255,
+            blue:  CGFloat((hexCodeEightDigits & 0x0000FF) >> 0)/255, alpha: alpha
+        ) // for instance: 0xfe5960
+    }
+}
 
 extension UIViewController {
     func generatingColorWithRGB(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: Float) -> UIColor {
@@ -23,3 +23,6 @@ extension UIViewController {
         return colorByRGB
     }
 }
+
+
+
